0a76870d82f3fec25ed011bae1f02a8299e5fe2c
Stein Martin Hustad <stein@hustad.com>
2013-04-27 23:16:21 +0200

    Incorrect url for pckeyboardhack

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,7 +4,7 @@
 # $app - location of installed application
 # $plist_path - location of plist
 class pckeyboardhack::config {
-  $dmg_url = 'http://pqrs.org/macosx/pckeyboardhack/files/PCKeyboardHack-9.0.0.dmg'
+  $dmg_url = 'http://pqrs.org/macosx/keyremap4macbook/files/PCKeyboardHack-9.0.0.dmg'
   $app = '/Applications/PCKeyboardHack.app'
   $plist_path = "/Users/${::boxen_user}/Library/Preferences/org.pqrs.PCKeyboardHack.plist"
 }
