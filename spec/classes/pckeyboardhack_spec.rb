eb34f5d16d469dd63c125cd35975076a00cb7ada
Stein Martin Hustad <stein@hustad.com>
2013-12-27 13:00:31 +0100

    Update to 10.4.0

--- spec/classes/pckeyboardhack_spec.rb
+++ spec/classes/pckeyboardhack_spec.rb
@@ -4,9 +4,9 @@ describe 'pckeyboardhack' do
   it do
     should include_class('pckeyboardhack::config')
 
-    should contain_package('PCKeyboardHack_10.0.0').with({
+    should contain_package('PCKeyboardHack_10.4.0').with({
       :ensure   => 'installed',
-      :source   => 'https://pqrs.org/macosx/keyremap4macbook/files/PCKeyboardHack-10.0.0.dmg',
+      :source   => 'https://pqrs.org/macosx/keyremap4macbook/files/PCKeyboardHack-10.4.0.dmg',
       :provider => 'pkgdmg'
     })
   end
